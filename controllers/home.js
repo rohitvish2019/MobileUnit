0a54cd8ca918c11229e12ee54b8aa090f084b7c6
Rohit Vishwakarma <rohit.vishwakarma@logicoy.com>
2024-02-07 14:54:59 +0530

    Home page

---
+++ controllers/home.js
@@ -0,0 +1,3 @@
+module.exports.homepage = function(req, res){
+    return res.render('home');
+}
--- mobile.js
+++ mobile.js
@@ -4,11 +4,14 @@ const express = require('express');
 // Creating an instance of Express
 const app = express();
 const PORT = 3000; // You can set any port number you like
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.set('views','./views');
+app.use(express.urlencoded({extended:true}));
 
+app.use(express.static('./public'));
 // Define a route for the homepage
-app.get('/', (req, res) => {
-  res.send('Hello, World!'); // Sends 'Hello, World!' as the response
-});
+app.use('/', require('./routes/index'))
 
 // Start the server
 app.listen(PORT, () => {
